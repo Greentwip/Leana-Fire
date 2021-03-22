84ffe279fc8ad1c2978e1f09ff27ad58e7e769f1
Greentwip <vjlopezcarrillo@gmail.com>
2021-03-21 18:18:31 -0600

    Camera zoom

--- Assets/Battle/CameraMouseFollow.cs
+++ Assets/Battle/CameraMouseFollow.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class CameraMouseFollow : MonoBehaviour
 {
     // Start is called before the first frame update
-    Camera camera;
+    //Camera camera;
+
+    public int Boundary = 50; // distance from edge scrolling starts
+    public int speed = 5;
+    private int theScreenWidth = 0;
+    private int theScreenHeight = 0;
+
     void Start()
     {
-        camera = GetComponent<Camera>();
+        //camera = GetComponent<Camera>();
+        theScreenWidth = Screen.width;
+        theScreenHeight = Screen.height;
+
     }
 
     // Update is called once per frame
@@ -16,6 +25,47 @@ public class CameraMouseFollow : MonoBehaviour
     {
         float xAxisValue = Input.GetAxis("Horizontal");
         float zAxisValue = Input.GetAxis("Vertical");
-        camera.transform.Translate(new Vector3(xAxisValue, zAxisValue, 0.0f));
+        transform.Translate(new Vector3(xAxisValue, zAxisValue, 0.0f));
+
+#if UNITY_EDITOR
+        if (Input.mousePosition.x <= 1 || Input.mousePosition.y <= 1 || Input.mousePosition.x >= UnityEditor.Handles.GetMainGameViewSize().x - 1 || Input.mousePosition.y >= UnityEditor.Handles.GetMainGameViewSize().y - 1) return;
+#else
+    if (Input.mousePosition.x <= 1 || Input.mousePosition.y <= 1 || Input.mousePosition.x >= Screen.width - 1 || Input.mousePosition.y >= Screen.height - 1) return;
+#endif
+
+
+        if (Input.mousePosition.x > theScreenWidth - Boundary)
+        {
+            transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f); // move on +X axis
+        }
+        if (Input.mousePosition.x < 0 + Boundary)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f); // move on +X axis
+        }
+        if (Input.mousePosition.y > theScreenHeight - Boundary)
+        {
+            transform.Translate(0.0f, speed * Time.deltaTime, 0.0f); // move on +Y axis
+        }
+        if (Input.mousePosition.y < 0 + Boundary)
+        {
+            transform.Translate(0.0f, -speed * Time.deltaTime, 0.0f); // move on +Y axis
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            GetComponent<Camera>().orthographicSize -= 10;
+            if(GetComponent<Camera>().orthographicSize <= 20)
+            {
+                GetComponent<Camera>().orthographicSize = 20;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            GetComponent<Camera>().orthographicSize += 10;
+            if (GetComponent<Camera>().orthographicSize >= 50)
+            {
+                GetComponent<Camera>().orthographicSize = 50;
+            }
+        }
     }
 }
